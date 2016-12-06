2c9d791eb59f767b8f0953c5462a67f56ab72c73
Nicolò Crisafulli <Nicol Crisafulli>
2016-12-06 12:02:11 +0100

    Aggiunto tema nei file

--- js/hr_br_example.js
+++ js/hr_br_example.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
 
     Highcharts.theme = {
         colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
@@ -202,35 +203,51 @@ $(document).ready(function(){
 // Apply the theme
     Highcharts.setOptions(Highcharts.theme);
 
+    var highHR = [];
+    var midHR = [];
+    var lowHR = [];
 
 
-   Highcharts.chart('diffcolor', {
+    highHR.push(185);
+
+    midHR.push(184,183,182,181,180,179,178,177,176,175,174,173,172,171,170,
+    169,168,167,166,165,164,163,162,161,160,159,158,157,156,155,154,153,152,151,
+    150,149,148,147,146,145,144,143,142,141);
+
+    lowHR.push(140);
+
+
+
+   Highcharts.chart('provola', {
+       chart: {
+           type: 'line'
+       },
        title: {
-            text: 'Prova multicolor line con HR'
+           text: 'Esempio con HR e BR'
        },
        xAxis: {
-           crosshair: {
-               snap: false
-           }
+           categories: [1,2,3,4,5,6,7,8,9,10]
        },
        yAxis: {
-           crosshair: {
-               snap: false
+           title: {
+               text: 'HR'
+           }
+       },
+       plotOptions: {
+           line: {
+               dataLabels: {
+                   enabled: true
+               },
+               enableMOuseTracking: false
            }
        },
        series: [{
            name: 'HR',
-           data: [140,146,148,150,164,170,179,176,177,180,181,174,172,185,185,
-           180,175,160,158,146,140],
-           zones: [{
-               value: 149,
-               color: '#47ed00'
-           }, {
-               value: 184,
-               color: '#ed7e00'
-           }, {
-               color: '#ed0019'
-           }]
+           data: [140,164,170,179,176,177,180,181,174,172]
+
+       }, {
+           name: 'BR',
+           data: [22,35,36,38,34,36,40,39,32,34]
        }]
    });
 });
